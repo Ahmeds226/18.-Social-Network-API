3c65b2f7254fe6ba06062f1881fcb4d96ab800cc
Ahmed Shahnawaz <iahmed.s1@icloud.com>
2022-09-06 14:46:21 +0100

    added to thought controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -17,6 +17,27 @@ const thoughtController = {
         res.sendStatus(400);
       });
   },
+  // get one thoughts by id
+  getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+      .populate({
+        path: "reactions",
+        select: "-__v",
+      })
+      .select("-__v")
+      .sort({ _id: -1 })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: "No thoughts found with that id!" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
 };
 
 module.exports = thoughtController;
