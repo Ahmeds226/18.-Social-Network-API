3af92da7075d6415a5ed2b4ea7bf907bbdd6f5d6
Ahmed Shahnawaz <iahmed.s1@icloud.com>
2022-09-07 01:35:35 +0100

    index.js completed

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+// Imports:
+const router = require("express").Router();
+const apiRoutes = require("./api");
+
+router.use("/api", apiRoutes);
+
+router.use((req, res) => {
+  res.status(404).send("<h1> Error code: 404 </h1>");
+});
+
+module.exports = router;
